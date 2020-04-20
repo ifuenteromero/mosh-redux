531c98888eb34204db4dc5041fe6edc8e8f0e34b
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2020-04-20 17:07:21 +0200

    add apuntes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log("Hello World!");
+//video 3
+
+function sayHello() {
+    return 'Hello World';
+};
+
+
+//Assign to a variable, pasamos una referencia de la función, no la invocamos
+let fn = sayHello; 
+//Son equivalentes
+fn();
+sayHello();
+
+//Pass as an argument
+function greet(fnMessage){
+    console.log(fnMessage())
+}
+
+function logHello() {
+    console.log('Hello');
+}
+
+function run(fn){
+    fn();
+}
+
+// run(logHello)
+
+//Return from other function
+
+function sayBye() {
+    return function() {
+        console.log('retorna función');
+    }
+}
+
+fn2 = sayBye;
+console.log({fn2})
+console.log(fn2())
+console.log(fn2()())
+fn2 = sayBye();
+console.log({fn2})
+
+//HIGHER ORDER FUNCTIONS
+let numbers = [1, 2, 3];
+//map y setTimeout es una higher order function porque toma una función como argumento
+numbers.map(number => number*2)
+
+setTimeout(() => console.log('Hello'), 1000);
+
+// FUNCTION COMPOSITION
+
+//NON FUNCTIONAL STYLE OF CODE
+let input = "  JavaScript  ";
+let output = `<div>${input.trim()}</div>`;
+console.log({output})
+
+//FUNCTIONAL STYLE
+//trim
+//wrap in div
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+const result = wrapInDiv(toLowerCase(trim('   Haskell   ')));
+console.log({result});
